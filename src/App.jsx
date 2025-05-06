637153d4e8850107402f30b4976e0b60974c6520
unknown <jaybhuptani1054@gmail.com>
2025-05-06 14:30:33 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+
 const App = () => {
-  // return <div className="">Template</div>;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    // Add 'return' here
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>HOME</div>;
+  return <div className="bg-red-200">HOME</div>;
 };
 
 export default Home;
